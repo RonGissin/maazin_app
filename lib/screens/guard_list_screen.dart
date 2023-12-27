32967cb24cab7fe36d21a6d190e78f6ac806a04d
Ron Gissin <rongissinnew@gmail.com>
2023-12-27 18:28:37 +0200

    fix bugs

--- lib/screens/guard_list_screen.dart
+++ lib/screens/guard_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/generate_list_modal.dart'; // Import the GenerateListModal class
 import 'package:maazin_app/guard_list_generator.dart';
 import 'package:maazin_app/models/assigned_team_member.dart';
 import '../models/team_member.dart';
@@ -10,14 +11,14 @@ import '../widgets/number_of_guards_input.dart';
 import 'package:flutter/services.dart';
 
 class GuardListScreen extends StatefulWidget {
-  const GuardListScreen({super.key});
+  const GuardListScreen({Key? key}) : super(key: key);
 
   @override
   _GuardListScreenState createState() => _GuardListScreenState();
 }
 
-class _GuardListScreenState extends State<GuardListScreen> 
-  with AutomaticKeepAliveClientMixin {
+class _GuardListScreenState extends State<GuardListScreen>
+    with AutomaticKeepAliveClientMixin {
   List<List<AssignedTeamMember>> guardGroups = [];
   List<TeamMember> teamMembers = [];
   GuardListGenerator generator = GuardListGenerator();
@@ -26,77 +27,96 @@ class _GuardListScreenState extends State<GuardListScreen>
   DateTime selectedStartTime = DateTime.now();
   DateTime selectedEndTime = DateTime.now();
   bool isInvalidTime = false;
-  int? intGuardTime ;
+  int? intGuardTime;
   TextEditingController doubleGuardTimeController = TextEditingController();
+  bool isFixedGuardTime = false;
 
   @override
   bool get wantKeepAlive => true;
 
-  void _showGenerateListModal(BuildContext context, DateTime previousStartTime, DateTime previousEndTime) {
+  void _showGenerateListModal(
+      BuildContext context, DateTime previousStartTime, DateTime previousEndTime) {
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
-        return _buildGenerateListModal(context, previousStartTime, previousEndTime);
+        return GenerateListModal(
+          onGenerateList: _generateList, // Pass the callback function
+          previousStartTime: previousStartTime,
+          previousEndTime: previousEndTime,
+        );
       },
     );
   }
 
-Widget _buildGenerateListModal(BuildContext context, DateTime previousStartTime, DateTime previousEndTime) {
-
+  Widget _buildGenerateListModal(
+      BuildContext context, DateTime previousStartTime, DateTime previousEndTime) {
     return Container(
-    padding: const EdgeInsets.all(16.0),     
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          'Select Start Time and End Time',
-          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: 16.0),
-        DateTimePicker(
-          label: 'Start Time',
-          initialTime: previousStartTime,
-          onTimeChanged: (DateTime time) {
-            setState(() {
-              selectedStartTime = time;
-            });
-          },
-        ),
-        const SizedBox(height: 16.0),
-        DateTimePicker(
-          label: 'End Time',
-          initialTime: previousEndTime,
-          onTimeChanged: (DateTime time) {
-            setState(() {             
-              selectedEndTime = time;
-
-              isInvalidTime = selectedStartTime.isAfter(selectedEndTime);
-            });
-          },
-        ),
-        const SizedBox(height: 16.0),
-        Visibility(
-          visible: isInvalidTime,
-          child: Text(
-            'End time cannot be earlier than start time',
-            style: TextStyle(color: Colors.red),
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Select Start Time and End Time',
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
           ),
-        ),
-        const SizedBox(height: 16.0),
-        NumberOfGuardsInput(
-          initialState: numberOfConcurrentGuards,
-          onChanged: (value) {
-            setState(() {
-              numberOfConcurrentGuards = value;
-            });
-          },
-        ),
-        const SizedBox(height: 16.0),
-            TextField(
+          const SizedBox(height: 10.0),
+          DateTimePicker(
+            label: 'Start Time',
+            initialTime: previousStartTime,
+            onTimeChanged: (DateTime time) {
+              setState(() {
+                selectedStartTime = time;
+              });
+            },
+          ),
+          const SizedBox(height: 10.0),
+          DateTimePicker(
+            label: 'End Time',
+            initialTime: previousEndTime,
+            onTimeChanged: (DateTime time) {
+              setState(() {
+                selectedEndTime = time;
+                isInvalidTime = selectedStartTime.isAfter(selectedEndTime);
+              });
+            },
+          ),
+          const SizedBox(height: 10.0),
+          Visibility(
+            visible: isInvalidTime,
+            child: Text(
+              'End time cannot be earlier than start time',
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          NumberOfGuardsInput(
+            initialState: numberOfConcurrentGuards,
+            onChanged: (value) {
+              setState(() {
+                numberOfConcurrentGuards = value;
+              });
+            },
+          ),
+          const SizedBox(height: 10.0),
+          Row(
+            children: [
+              Switch( // Change Checkbox to Switch
+                value: isFixedGuardTime,
+                onChanged: (value) {
+                  setState(() {
+                    isFixedGuardTime = value;
+                  });
+                },
+              ),
+              Text('Fixed Guard Time'),
+            ],
+          ),
+          Visibility(
+            visible: isFixedGuardTime,
+            child: TextField(
               controller: doubleGuardTimeController,
               keyboardType: TextInputType.number,
               onChanged: (value) {
-                // Update the stored value whenever the TextField changes
                 setState(() {
                   intGuardTime = int.tryParse(value);
                 });
@@ -106,41 +126,71 @@ Widget _buildGenerateListModal(BuildContext context, DateTime previousStartTime,
                 hintText: 'Enter guard time in minutes',
               ),
             ),
-        ElevatedButton(
-          onPressed: () {
-            setState(() {
-            isInvalidTime = selectedStartTime.isAfter(selectedEndTime);
-            });
-             // Perform validation
-            if (isInvalidTime) {
-              return;
-            }
+          ),
+          Center(
+            child: ElevatedButton(
+            onPressed: () {
+              setState(() {
+                isInvalidTime = selectedStartTime.isAfter(selectedEndTime);
+              });
+              // Perform validation
+              if (isInvalidTime) {
+                return;
+              }
 
-            // Perform list generation logic here
-            setState(() {
-              guardGroups = generator.generateGuardGroups(
-                teamMembers,
-                numberOfConcurrentGuards,
-                selectedStartTime,
-                selectedEndTime,
-                intGuardTime
-                
-              );
-            });
-            Navigator.pop(context); // Close the modal
-          },
-          child: const Text('Generate'),
-        ),
-      ],
-    ),
-  );
-}
+              // Perform list generation logic here
+              setState(() {
+                guardGroups = generator.generateGuardGroups(
+                  teamMembers,
+                  numberOfConcurrentGuards,
+                  selectedStartTime,
+                  selectedEndTime,
+                  isFixedGuardTime ? intGuardTime : null,
+                );
+              });
+              Navigator.pop(context); // Close the modal
+            },
+            child: const Text('Generate'),
+          )),
+        ],
+      ),
+    );
+  }
 
-void _showSnackBar(BuildContext context, String message) {
+  void _generateList(
+    DateTime startTime,
+    DateTime endTime,
+    int? guardTime,
+    int numberOfConcurrentGuards,
+    bool isFixedGuardTime, // Add this parameter
+  ) {
+    setState(() {
+      isInvalidTime = startTime.isAfter(endTime);
+    });
+
+    // Perform validation
+    if (isInvalidTime) {
+      return;
+    }
+
+    // Perform list generation logic here
+    setState(() {
+      guardGroups = generator.generateGuardGroups(
+        teamMembers,
+        numberOfConcurrentGuards,
+        startTime,
+        endTime,
+        isFixedGuardTime ? guardTime : null,
+      );
+    });
+    Navigator.pop(context); // Close the modal
+  }
+
+  void _showSnackBar(BuildContext context, String message) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Padding(
-          padding: const EdgeInsets.only(bottom: 25.0), // Adjust bottom padding as needed
+          padding: const EdgeInsets.only(bottom: 25.0),
           child: Center(child: Text(message)),
         ),
         duration: Duration(seconds: 2),
@@ -151,10 +201,10 @@ void _showSnackBar(BuildContext context, String message) {
     );
   }
 
-@override
-Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     super.build(context);
-    
+
     Color secondaryColor = Theme.of(context).colorScheme.secondary;
     teamMembers = Provider.of<TeamProvider>(context).teamMembers;
     guardGroupsList = GuardGroupsList(guardGroups: guardGroups);
@@ -163,7 +213,7 @@ Widget build(BuildContext context) {
       appBar: AppBar(
         title: Text(
           'Guard List',
-          style: TextStyle(color: secondaryColor)
+          style: TextStyle(color: secondaryColor),
         ),
       ),
       body: guardGroupsList,
@@ -173,28 +223,30 @@ Widget build(BuildContext context) {
           Padding(
             padding: const EdgeInsets.only(right: 16.0),
             child: FloatingActionButton(
-            onPressed: () {
-              // Handle second FAB press
-              Clipboard.setData(ClipboardData(text: guardGroupsList.getReadableList()));
-              _showSnackBar(context, 'Copied!');
-            },
-            child: const Icon(Icons.copy),
-          ),),
-          
-          FloatingActionButton(
-            onPressed: () {
-              _showGenerateListModal(context, _calculatePresentionTime(selectedStartTime), _calculatePresentionTime(selectedEndTime));
-            },
-            child: const Icon(Icons.edit)
+              onPressed: () {
+                Clipboard.setData(
+                    ClipboardData(text: guardGroupsList.getReadableList()));
+                _showSnackBar(context, 'Copied!');
+              },
+              child: const Icon(Icons.copy),
+            ),
           ),
+          FloatingActionButton(
+              onPressed: () {
+                _showGenerateListModal(context,
+                    _calculatePresentionTime(selectedStartTime),
+                    _calculatePresentionTime(selectedEndTime));
+              },
+              child: const Icon(Icons.edit)),
           const SizedBox(height: 16.0),
         ],
       ),
     );
   }
-  
-  DateTime _calculatePresentionTime(DateTime startTime) 
-  {
-    return startTime.difference(DateTime.now()) < Duration.zero ? DateTime.now():startTime;
+
+  DateTime _calculatePresentionTime(DateTime startTime) {
+    return startTime.difference(DateTime.now()) < Duration.zero
+        ? DateTime.now()
+        : startTime;
   }
 }
---
+++ lib/widgets/generate_list_modal.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import './datetime_picker.dart';
+import './number_of_guards_input.dart';
+
+class GenerateListModal extends StatefulWidget {
+  final DateTime previousStartTime;
+  final DateTime previousEndTime;
+  final Function(DateTime, DateTime, int?, int, bool) onGenerateList;
+
+  const GenerateListModal({
+    Key? key,
+    required this.previousStartTime,
+    required this.previousEndTime,
+    required this.onGenerateList,
+  }) : super(key: key);
+
+  @override
+  _GenerateListModalState createState() => _GenerateListModalState();
+}
+
+class _GenerateListModalState extends State<GenerateListModal> {
+  DateTime selectedStartTime = DateTime.now();
+  DateTime selectedEndTime = DateTime.now();
+  bool isInvalidTime = false;
+  int numberOfConcurrentGuards = 1;
+  int? intGuardTime;
+  TextEditingController doubleGuardTimeController = TextEditingController();
+  bool isFixedGuardTime = false;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedStartTime = widget.previousStartTime;
+    selectedEndTime = widget.previousEndTime;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Select Start Time and End Time',
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10.0),
+          DateTimePicker(
+            label: 'Start Time',
+            initialTime: widget.previousStartTime,
+            onTimeChanged: (DateTime time) {
+              setState(() {
+                selectedStartTime = time;
+              });
+            },
+          ),
+          const SizedBox(height: 10.0),
+          DateTimePicker(
+            label: 'End Time',
+            initialTime: widget.previousEndTime,
+            onTimeChanged: (DateTime time) {
+              setState(() {
+                selectedEndTime = time;
+                isInvalidTime = selectedStartTime.isAfter(selectedEndTime);
+              });
+            },
+          ),
+          const SizedBox(height: 10.0),
+          Visibility(
+            visible: isInvalidTime,
+            child: Text(
+              'End time cannot be earlier than start time',
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          NumberOfGuardsInput(
+            initialState: numberOfConcurrentGuards,
+            onChanged: (value) {
+              setState(() {
+                numberOfConcurrentGuards = value;
+              });
+            },
+          ),
+          const SizedBox(height: 10.0),
+          Row(
+            children: [
+              Switch(
+                value: isFixedGuardTime,
+                onChanged: (value) {
+                  setState(() {
+                    isFixedGuardTime = value;
+                  });
+                },
+              ),
+              Text('Fixed Guard Time'),
+            ],
+          ),
+          Visibility(
+            visible: isFixedGuardTime,
+            child: TextField(
+              controller: doubleGuardTimeController,
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  intGuardTime = int.tryParse(value);
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Guard Time in minutes - optional',
+                hintText: 'Enter guard time in minutes',
+              ),
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  isInvalidTime = selectedStartTime.isAfter(selectedEndTime);
+                });
+                // Perform validation
+                if (isInvalidTime) {
+                  return;
+                }
+
+                widget.onGenerateList(
+                  selectedStartTime,
+                  selectedEndTime,
+                  isFixedGuardTime ? intGuardTime : null,
+                  numberOfConcurrentGuards,
+                  isFixedGuardTime
+                );
+              },
+              child: const Text('Generate'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

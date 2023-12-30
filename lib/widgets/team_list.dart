b35119f5f8015831c7ac37d199340463dd7eaacd
Ron Gissin <rongissinnew@gmail.com>
2023-12-30 17:13:17 +0200

    slide delete

--- lib/widgets/team_list.dart
+++ lib/widgets/team_list.dart
@@ -4,7 +4,6 @@ import '../team_provider.dart';
 import '../models/team_member.dart';
 import '../widgets/modify_team_member_dialog.dart';
 
-
 class TeamList extends StatefulWidget {
   const TeamList({Key? key}) : super(key: key);
 
@@ -13,7 +12,6 @@ class TeamList extends StatefulWidget {
 }
 
 class _TeamListState extends State<TeamList> {
-
   @override
   Widget build(BuildContext context) {
     List<TeamMember> teamMembers = Provider.of<TeamProvider>(context).teamMembers;
@@ -24,89 +22,85 @@ class _TeamListState extends State<TeamList> {
         itemBuilder: (context, index) {
           TeamMember teamMember = teamMembers[index];
 
-          return Card(
-            child: Opacity(
-              opacity: teamMember.isEnabled ? 1.0 : 0.5, // Adjust the opacity value as needed
-              child: ListTile(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Expanded(child: Text(teamMember.name)),
-                  if (teamMember.isEnabled)
-                    IconButton(
-                      icon: Icon(Icons.done),
-                      onPressed: () {
-                        teamMember.isEnabled = false;
-                        Provider.of<TeamProvider>(context, listen: false).saveTeamMembers();
-                        setState(() => {});
-                      },
+          return Dismissible(
+            key: Key(teamMember.name),
+            direction: DismissDirection.startToEnd, // Changed direction
+            background: Container(
+              color: Colors.white,
+              alignment: Alignment.centerLeft, // Align to the left
+              child: Material(
+                elevation: 1.0,
+                borderRadius: BorderRadius.circular(10.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0), // Rounded corners
+                  child: Container(
+                    height: 65.0,
+                    color: Colors.red,
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.only(left: 16.0),
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
                     ),
-                  if (!teamMember.isEnabled)
-                    IconButton(
-                      icon: Icon(Icons.do_not_disturb),
-                      onPressed: () {
-                        teamMember.isEnabled = true;
-                        Provider.of<TeamProvider>(context, listen: false).saveTeamMembers();
-                        setState(() => {});
-                      },
-                    ),
-                  IconButton(
-                    icon: Icon(Icons.edit),
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return ModifyTeamMemberDialog(
-                            teamMembers: teamMembers,
-                            title: 'Edit Team Member',
-                            actionButtonText: 'Edit',
-                            onActionButtonPressed: (String newName) =>
-                                Provider.of<TeamProvider>(context, listen: false)
-                                    .editTeamMember(teamMember.name, newName),
+                  ),
+                ),
+              ),
+            ),
+            onDismissed: (direction) {
+              Provider.of<TeamProvider>(context, listen: false).removeTeamMember(index);
+            },
+            child: Card(
+              child: Opacity(
+                opacity: teamMember.isEnabled ? 1.0 : 0.5,
+                child: ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Expanded(child: Text(teamMember.name)),
+                      if (teamMember.isEnabled)
+                        IconButton(
+                          icon: Icon(Icons.done),
+                          onPressed: () {
+                            teamMember.isEnabled = false;
+                            Provider.of<TeamProvider>(context, listen: false).saveTeamMembers();
+                            setState(() {});
+                          },
+                        ),
+                      if (!teamMember.isEnabled)
+                        IconButton(
+                          icon: Icon(Icons.do_not_disturb),
+                          onPressed: () {
+                            teamMember.isEnabled = true;
+                            Provider.of<TeamProvider>(context, listen: false).saveTeamMembers();
+                            setState(() {});
+                          },
+                        ),
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return ModifyTeamMemberDialog(
+                                teamMembers: teamMembers,
+                                title: 'Edit Team Member',
+                                actionButtonText: 'Edit',
+                                onActionButtonPressed: (String newName) =>
+                                    Provider.of<TeamProvider>(context, listen: false)
+                                        .editTeamMember(teamMember.name, newName),
+                              );
+                            },
                           );
                         },
-                      );
-                    },
+                      ),
+                    ],
                   ),
-                  IconButton(
-                    icon: Icon(Icons.remove),
-                    onPressed: () {
-                      _showDeleteConfirmationDialog(context, index, teamMember.name);
-                    },
-                  ),
-                ],
+                ),
               ),
-            )),
+            ),
           );
         },
       ),
-    );
-  }
-
-  void _showDeleteConfirmationDialog(BuildContext context, int teamMemberIndex, String teamMemberName) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          content: Text('Are you sure you want to delete $teamMemberName from the team?'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
-              },
-              child: Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                // Delete the team member
-                Provider.of<TeamProvider>(context, listen: false).removeTeamMember(teamMemberIndex);
-                Navigator.of(context).pop(); // Close the dialog
-              },
-              child: Text('Delete'),
-            ),
-          ],
-        );
-      },
     );
   }
 }

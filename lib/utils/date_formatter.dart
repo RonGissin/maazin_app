28fafa6505086544fca43048e4fca0fa6e2231bc
Omer Mar Chaim <omer.marchaim@wsc-sports.com>
2024-02-05 20:28:15 +0200

    Add date in case it change in copied list

--- lib/utils/date_formatter.dart
+++ lib/utils/date_formatter.dart
@@ -4,4 +4,9 @@ class DateFormatter {
         '${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}';
     return formattedTime;
   }
+  static String formatDate(DateTime dateTime) {
+    final String formattedDate =
+        '${dateTime.day.toString().padLeft(2, '0')}/${dateTime.month.toString().padLeft(2, '0')}';
+    return formattedDate;
+  }
 }
--- lib/utils/whatsapp_guard_list_formatter.dart
+++ lib/utils/whatsapp_guard_list_formatter.dart
@@ -4,9 +4,23 @@ import '../models/assigned_team_member.dart';
 class WhatsappGuardListFormatter {
   static String ListToString(List<List<AssignedTeamMember>> guardGroups) {
     StringBuffer formattedList = StringBuffer();
+    int currentDay= guardGroups[0][0].startTime.day;
+    int cuttentMonth= guardGroups[0][0].startTime.month;
     formattedList.writeln('*Guard List*'); // WhatsApp markdown for bold text
+    formattedList.writeln(DateFormatter.formatDate(guardGroups[0][0].startTime)); // WhatsApp markdown for bold text
 
     for (int i = 0; i < guardGroups.length; i++) {
+
+       int tempDay = guardGroups[i][0].startTime.day;
+       int tempMonth = guardGroups[i][0].startTime.month;
+
+      if (tempDay > currentDay || tempMonth > cuttentMonth)
+      {
+        currentDay = tempDay;
+        cuttentMonth=tempMonth;
+        formattedList.writeln('${DateFormatter.formatDate(guardGroups[i][0].startTime)}'); // WhatsApp markdown for bold text
+      }
+
       formattedList.write('- ');
       for (int j = 0; j < guardGroups[i].length; j++) {
         formattedList.write('${guardGroups[i][j].name}');
@@ -14,7 +28,8 @@ class WhatsappGuardListFormatter {
           formattedList.write(', ');
         }
       }
-      formattedList.write(' - ${DateFormatter.formatTime(guardGroups[i][0].startTime)} to ${DateFormatter.formatTime(guardGroups[i][0].endTime)}\n');
+     
+      formattedList.write(' : ${DateFormatter.formatTime(guardGroups[i][0].startTime)} - ${DateFormatter.formatTime(guardGroups[i][0].endTime)}\n');
     }
 
     return formattedList.toString();
